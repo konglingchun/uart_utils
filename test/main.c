e37b6f7be5c959e952a6fc87621268563351df78
konglingchun <konglc@idatachina.com>
2017-08-22 11:06:25 +0800

    update test demo

--- test/main.c
+++ test/main.c
@@ -13,12 +13,11 @@ int main(int argc, char *argv[])
 	int receive_length;
 	char receive_buffer[1024] = "";
 
-	uart_fd = uart_init("/dev/ttyUSB0", 9600, 8, 1, 'E', 0);
+	uart_fd = uart_init("/dev/ttyUSB0", 9600, 8, 1, 'N', 0);
 	while(1){		
-		receive_length = read(uart_fd, receive_buffer, sizeof(receive_buffer));
-		if(receive_length < 0) {
-			perror("read");
-		} else if(receive_length > 0){
+		receive_length = uart_read_until(uart_fd, receive_buffer, sizeof(receive_buffer), '\n', 50);
+		if(receive_length > 0){
+			print_buffer("", receive_buffer, receive_length);
 			print_buffer_hex("", receive_buffer, receive_length);
 		}
 	}
