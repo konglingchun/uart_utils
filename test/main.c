0c9eb5d13c2c97671702b5b997110c574cd752e5
konglingchun <konglc@idatachina.com>
2017-11-07 10:01:02 +0800

    change device attribute

--- test/main.c
+++ test/main.c
@@ -16,11 +16,13 @@ int main(int argc, char *argv[])
 
 	if(argc == 2
 		&& strstr(argv[1], "/dev/tty")){
-		uart_fd = uart_init(argv[1], 38400, 8, 1, 'N', 0);
+		uart_fd = uart_init(argv[1], 115200, 8, 1, 'N', 0);
 		while(1){
 			ret = uart_read_until_time(uart_fd, buffer, sizeof(buffer), 500, 10);
 			if(ret > 0){
-				print_buffer_hex("receive monitor", buffer, ret);
+				print_buffer("receive monitor1", buffer, ret);
+				print_buffer_char_index("receive monitor2", buffer, ret);
+				print_buffer_hex_index("receive monitor3", buffer, ret);
 			}
 		}
 	}else{
