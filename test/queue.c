6bd0fd05518b501d073eb4bdf25596654ae7bb5d
kong_ling_chun <kong_ling_chun@sina.com>
2020-03-26 14:31:33 +0800

    Test code that matches after fixing the queue_buffer bug

--- test/queue.c
+++ test/queue.c
@@ -7,56 +7,78 @@
 void queue_test_0(void)
 {
 	_buffer_queue_t handler;
-	char data_arry[256] = "";
-	char data_enqueue[] = "bcdef";
-	char data_dequeue[256] = "";
+	char data_arry[128] = "";
+	char data_enqueue[] = "abcdef";
+	char data_dequeue[128] = "";
 	int data_size = 0;
-	
+
 	_buffer_queue_init(&handler, 10, QUEUE_CHAR);
-	printf("queue_size left = %d\n", _buffer_queue_size_left(&handler));
+	printf("queue_size left = %d\r\n", _buffer_queue_size_left(&handler));
 	
 	_buffer_queue_enqueue(&handler, "0");
+	printf("queue_size left = %d\r\n", _buffer_queue_size_left(&handler));
 	_buffer_queue_enqueue(&handler, "1");
+	printf("queue_size left = %d\r\n", _buffer_queue_size_left(&handler));
 	_buffer_queue_enqueue(&handler, "2");
+	printf("queue_size left = %d\r\n", _buffer_queue_size_left(&handler));
 	_buffer_queue_enqueue(&handler, "3");
+	printf("queue_size left = %d\r\n", _buffer_queue_size_left(&handler));
 	_buffer_queue_enqueue(&handler, "4");
+	printf("queue_size left = %d\r\n", _buffer_queue_size_left(&handler));
 	_buffer_queue_enqueue(&handler, "5");
+	printf("queue_size left = %d\r\n", _buffer_queue_size_left(&handler));
 	_buffer_queue_enqueue(&handler, "6");
+	printf("queue_size left = %d\r\n", _buffer_queue_size_left(&handler));
 	_buffer_queue_enqueue(&handler, "7");
+	printf("queue_size left = %d\r\n", _buffer_queue_size_left(&handler));
 	_buffer_queue_enqueue(&handler, "8");
+	printf("queue_size left = %d\r\n", _buffer_queue_size_left(&handler));
 	_buffer_queue_enqueue(&handler, "9");
-	_buffer_queue_enqueue(&handler, "a");
+	printf("queue_size left = %d\r\n", _buffer_queue_size_left(&handler));
+	_buffer_queue_enqueue(&handler, "A");
+	printf("queue_size left = %d\r\n", _buffer_queue_size_left(&handler));
 	
 	//test 0
 	data_size = _buffer_queue_size(&handler);
+	memset(data_arry, 0, sizeof(data_arry));
 	_buffer_queue_ergodic_buffer(&handler, data_arry, 0, data_size);
-	print_buffer("test 0: ergodic_buffer ", data_arry, data_size);
+	print_buffer("test 0: ergodic_buffer", data_arry, data_size);
 	
 	
 	//test 1
-	_buffer_queue_enqueue_flush(&handler, "a");
+	_buffer_queue_enqueue(&handler, "B");
 	data_size = _buffer_queue_size(&handler);
+	memset(data_arry, 0, sizeof(data_arry));
 	_buffer_queue_ergodic_buffer(&handler, data_arry, 0, data_size);
-	print_buffer("test 1: enqueue_flush ", data_arry, data_size);
-	
+	print_buffer("test 1-1: queue_enqueue", data_arry, data_size);
+
+	_buffer_queue_enqueue(&handler, "C");
+	data_size = _buffer_queue_size(&handler);
+	memset(data_arry, 0, sizeof(data_arry));
+	_buffer_queue_ergodic_buffer(&handler, data_arry, 0, data_size);
+	print_buffer("test 1-2: queue_enqueue", data_arry, data_size);
+		
 	//test 2
 	_buffer_queue_flush(&handler, 4);
 	data_size = _buffer_queue_size(&handler);
+	memset(data_arry, 0, sizeof(data_arry));
 	_buffer_queue_ergodic_buffer(&handler, data_arry, 0, data_size);
-	print_buffer("test 2: queue_flush ", data_arry, data_size);
+	print_buffer("test 2: ququq_flush", data_arry, data_size);
 	
 	//test 3
 	_buffer_queue_enqueue_multi(&handler, data_enqueue, strlen(data_enqueue));
 	data_size = _buffer_queue_size(&handler);
+	memset(data_arry, 0, sizeof(data_arry));
 	_buffer_queue_ergodic_buffer(&handler, data_arry, 0, data_size);
-	print_buffer("test 3: enqueue_multi ", data_arry, data_size);
+	print_buffer("test 3: enqueue_multi", data_arry, data_size);
 	
 	//test 4
 	_buffer_queue_dequeue_multi(&handler, data_dequeue, 5);
 	data_size = _buffer_queue_size(&handler);
+	memset(data_arry, 0, sizeof(data_arry));
 	_buffer_queue_ergodic_buffer(&handler, data_arry, 0, data_size);
-	print_buffer("test 4-1: dequeue_multi ", data_dequeue, 5);
-	print_buffer("test 4-2: ergodic_buffer ", data_arry, data_size);
+	print_buffer("test 4-1: dequeue_multi", data_dequeue, 5);
+	print_buffer("test 4-2: ergodic_buffer", data_arry, data_size);
 	
 	_buffer_queue_uninit(&handler);
 }
@@ -78,31 +100,31 @@ void queue_test_1(void)
 	printf("queue_size left = %d\n", _buffer_queue_size_left(&handler));
 
 	a.x = 0; a.y = 0;
-	_buffer_queue_enqueue_flush(&handler, &a);
+	_buffer_queue_enqueue(&handler, &a);
 	a.x = 1; a.y = 1;
-	_buffer_queue_enqueue_flush(&handler, &a);
+	_buffer_queue_enqueue(&handler, &a);
 	a.x = 2; a.y = 2;
-	_buffer_queue_enqueue_flush(&handler, &a);
+	_buffer_queue_enqueue(&handler, &a);
 	a.x = 3; a.y = 3;
-	_buffer_queue_enqueue_flush(&handler, &a);
+	_buffer_queue_enqueue(&handler, &a);
 	a.x = 4; a.y = 4;
-	_buffer_queue_enqueue_flush(&handler, &a);	
+	_buffer_queue_enqueue(&handler, &a);	
 	a.x = 5; a.y = 5;
-	_buffer_queue_enqueue_flush(&handler, &a);
+	_buffer_queue_enqueue(&handler, &a);
 	a.x = 6; a.y = 6;
-	_buffer_queue_enqueue_flush(&handler, &a);
+	_buffer_queue_enqueue(&handler, &a);
 	a.x = 7; a.y = 7;
-	_buffer_queue_enqueue_flush(&handler, &a);
+	_buffer_queue_enqueue(&handler, &a);
 	a.x = 8; a.y = 8;
-	_buffer_queue_enqueue_flush(&handler, &a);
+	_buffer_queue_enqueue(&handler, &a);
 	a.x = 9; a.y = 9;
-	_buffer_queue_enqueue_flush(&handler, &a);
+	_buffer_queue_enqueue(&handler, &a);
 	a.x = 10; a.y = 10;
-	_buffer_queue_enqueue_flush(&handler, &a);
+	_buffer_queue_enqueue(&handler, &a);
 	a.x = 11; a.y = 11;
-	_buffer_queue_enqueue_flush(&handler, &a);
+	_buffer_queue_enqueue(&handler, &a);
 	a.x = 12; a.y = 12;
-	_buffer_queue_enqueue_flush(&handler, &a);
+	_buffer_queue_enqueue(&handler, &a);
 	
 	data_size = _buffer_queue_size(&handler);
 
